e5a7dd002b034c41c896db744aff2f1b3696e629
Phillip McClure <23pmcclure@athenian.org>
2021-05-11 10:45:19 -0700

    hello

--- src/MolarMass.java
+++ src/MolarMass.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+import java.io.File;
 public class MolarMass {
+    private final File list;
+    private final Scanner scan;
+    private double molarMass;
+    public MolarMass() {
 
+    }
 }
